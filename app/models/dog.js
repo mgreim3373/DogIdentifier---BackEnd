b94f6ef528762b41a9470c87d2ef344dd91d89a1
mgreim3373 <mgreim3373@gmail.com>
2018-10-05 21:44:31 -0400

    Create Dog Mongoose Schema -created dog schema -requires image url, and string description -plan on making future additions

---
+++ app/models/dog.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const dogSchema = new mongoose.Schema({
+  image: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  timestamps: true
+})
+
+module.exports = mongoose.model('Dog', dogSchema)
