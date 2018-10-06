5a64b57329f4c3ed525f1747ec18c75dfdbde4b2
mgreim3373 <mgreim3373@gmail.com>
2018-10-05 22:18:15 -0400

    Create CRUD Routes -Created CRUD routes for dog data

--- app/routes/dog-routes.js
+++ app/routes/dog-routes.js
@@ -67,28 +67,26 @@ router.get('/dogs', requireToken, (req, res) => {
 
 // SHOW
 // GET /examples/5a7db6c74d55bc51bdf39793
-router.get('/examples/:id', requireToken, (req, res) => {
+router.get('/dogs/:id', requireToken, (req, res) => {
   // req.params.id will be set based on the `:id` in the route
-  Example.findById(req.params.id)
+  Dog.findById(req.params.id)
     .then(handle404)
     // if `findById` is succesful, respond with 200 and "example" JSON
-    .then(example => res.status(200).json({ example: example.toObject() }))
+    .then(dog => res.status(200).json({ dog: dog.toObject() }))
     // if an error occurs, pass it to the handler
     .catch(err => handle(err, res))
 })
 
-// Google Cloud Bucket
-
 // CREATE
 // POST /examples
-router.post('/examples', requireToken, (req, res) => {
+router.post('/dogs', requireToken, (req, res) => {
   // set owner of new example to be current user
-  req.body.example.owner = req.user.id
+  req.body.dogs.owner = req.user.id
 
-  Example.create(req.body.example)
-    // respond to succesful `create` with status 201 and JSON of new "example"
-    .then(example => {
-      res.status(201).json({ example: example.toObject() })
+  Dog.create(req.body.dog)
+    // respond to succesful `create` with status 201 and JSON of new "dog"
+    .then(dog => {
+      res.status(201).json({ dog: dog.toObject() })
     })
     // if an error occurs, pass it off to our error handler
     // the error handler needs the error message and the `res` object so that it
@@ -98,29 +96,29 @@ router.post('/examples', requireToken, (req, res) => {
 
 // UPDATE
 // PATCH /examples/5a7db6c74d55bc51bdf39793
-router.patch('/examples/:id', requireToken, (req, res) => {
+router.patch('/dogs/:id', requireToken, (req, res) => {
   // if the client attempts to change the `owner` property by including a new
   // owner, prevent that by deleting that key/value pair
-  delete req.body.example.owner
+  delete req.body.dog.owner
 
-  Example.findById(req.params.id)
+  Dog.findById(req.params.id)
     .then(handle404)
-    .then(example => {
+    .then(dog => {
       // pass the `req` object and the Mongoose record to `requireOwnership`
       // it will throw an error if the current user isn't the owner
-      requireOwnership(req, example)
+      requireOwnership(req, dog)
 
       // the client will often send empty strings for parameters that it does
       // not want to update. We delete any key/value pair where the value is
       // an empty string before updating
-      Object.keys(req.body.example).forEach(key => {
-        if (req.body.example[key] === '') {
-          delete req.body.example[key]
+      Object.keys(req.body.dog).forEach(key => {
+        if (req.body.dog[key] === '') {
+          delete req.body.dog[key]
         }
       })
 
       // pass the result of Mongoose's `.update` to the next `.then`
-      return example.update(req.body.example)
+      return dog.update(req.body.dog)
     })
     // if that succeeded, return 204 and no JSON
     .then(() => res.sendStatus(204))
@@ -130,14 +128,14 @@ router.patch('/examples/:id', requireToken, (req, res) => {
 
 // DESTROY
 // DELETE /examples/5a7db6c74d55bc51bdf39793
-router.delete('/examples/:id', requireToken, (req, res) => {
-  Example.findById(req.params.id)
+router.delete('/dogs/:id', requireToken, (req, res) => {
+  Dog.findById(req.params.id)
     .then(handle404)
-    .then(example => {
-      // throw an error if current user doesn't own `example`
-      requireOwnership(req, example)
+    .then(dog => {
+      // throw an error if current user doesn't own `dog`
+      requireOwnership(req, dog)
       // delete the example ONLY IF the above didn't throw
-      example.remove()
+      dog.remove()
     })
     // send back 204 and no content if the deletion succeeded
     .then(() => res.sendStatus(204))
