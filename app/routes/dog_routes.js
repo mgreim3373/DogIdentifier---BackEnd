888d1e66b47b92f37f787ff0e3593044c0515751
mgreim3373 <mgreim3373@gmail.com>
2018-10-25 14:03:47 -0400

    Commented code in dog_routes -commented google vision api response data filters

--- app/routes/dog_routes.js
+++ app/routes/dog_routes.js
@@ -65,16 +65,17 @@ router.get('/dogs', requireToken, (req, res) => {
 router.get('/dogs/:id', requireToken, (req, res) => {
   // req.params.id will be set based on the `:id` in the route
   Dog.findById(req.params.id)
-  .then(handle404)
+    .then(handle404)
   // if `findById` is succesful, respond with 200 and "example" JSON
-  .then(dog => res.status(200).json({ dog: dog.toObject() }))
+    .then(dog => res.status(200).json({ dog: dog.toObject() }))
   // if an error occurs, pass it to the handler
-  .catch(err => handle(err, res))
+    .catch(err => handle(err, res))
 })
 
 // CREATE
 // POST /examples
 router.post('/dogs', requireToken, (req, res) => {
+  // pulls image from post request to server and sends post request to google API
   const image = req.body.dogs.image
   axios({
     method: 'post',
@@ -98,6 +99,7 @@ router.post('/dogs', requireToken, (req, res) => {
   }).then(function (response) {
     const labels = response.data.responses[0].labelAnnotations
     const descripProbMap = labels.map(label => ({ description: label.description, probability: label.score }))
+    // Check if array of label objects contains any descriptions with the value dog
     const isDog = function () {
       if (descripProbMap.some(label => label.description === 'dog')) {
         return descripProbMap
@@ -105,21 +107,24 @@ router.post('/dogs', requireToken, (req, res) => {
         return {description: 'Not a dog!'}
       }
     }
+    // maps through array of known dog breeds and changes all to lower case to match
+    // vision api response
     const dogBreedLowerCase = dogBreed.map(breed => breed.toLowerCase())
-
+    // function to check if description value from google vision is a known
+    // dog breed
     const breedChecker = function (label) {
       if (dogBreedLowerCase.some(breed => breed === label.description)) {
         return label
       }
     }
-console.log('firstdog', isDog())
+    // maps through array of label objects and return key pairs
+    // where the key value is a known dog breed
     const breedCheckerMap = function () {
       if (isDog().description !== 'Not a dog!') {
         return isDog().map(label => breedChecker(label))
       }
     }
-    console.log('seconddog', breedCheckerMap())
-
+    // maps through array of label objects and removes undefined objects
     const breedCheckerUndefinedFilter = function () {
       if (isDog().description !== 'Not a dog!') {
         return breedCheckerMap().filter(breed => breed !== undefined)
@@ -127,20 +132,20 @@ console.log('firstdog', isDog())
         return isDog()
       }
     }
-
+    // maps through label keys and values and capitalizes all first letters
     const capitalizer = function (breed) {
       return breed.toLowerCase()
         .split(' ')
         .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
         .join(' ')
     }
-
+    // maps through processed array of label objects and values to be added to new dog
     const dogDataDisplayReady = function () {
-      if (breedCheckerUndefinedFilter().length == 0) {
+      if (breedCheckerUndefinedFilter().length === 0) {
         return {description: 'Unknown Dog'}
-      } else if (breedCheckerUndefinedFilter().description == 'Not a dog!') {
-        return breedCheckerUndefinedFilter()}
-      else {
+      } else if (breedCheckerUndefinedFilter().description === 'Not a dog!') {
+        return breedCheckerUndefinedFilter()
+      } else {
         const capitalizedBreed = breedCheckerUndefinedFilter()
           .map((breed) => (
             {
@@ -151,32 +156,30 @@ console.log('firstdog', isDog())
         return capitalizedBreed
       }
     }
-console.log('forthconddog', dogDataDisplayReady())
+    console.log('response4', dogDataDisplayReady())
     // set owner of new example to be current user
     req.body.dogs.owner = req.user.id
     req.body.dogs.label = dogDataDisplayReady()
     Dog.create(req.body.dogs)
     // respond to succesful `create` with status 201 and JSON of new "dog"
-    .then(dog => {
-      res.status(201).json({ dogs: dog.toObject() })
-    })
+      .then(dog => {
+        res.status(201).json({ dogs: dog.toObject() })
+      })
     // if an error occurs, pass it off to our error handler
     // the error handler needs the error message and the `res` object so that it
     // can send an error message back to the client
-    .catch(err => handle(err, res))
+      .catch(err => handle(err, res))
   })
+})
 
-  })
+// UPDATE
+// PATCH /examples/5a7db6c74d55bc51bdf39793
+router.patch('/dogs/:id', requireToken, (req, res) => {
+  // if the client attempts to change the `owner` property by including a new
+  // owner, prevent that by deleting that key/value pair
+  delete req.body.dog.owner
 
-
-  // UPDATE
-  // PATCH /examples/5a7db6c74d55bc51bdf39793
-  router.patch('/dogs/:id', requireToken, (req, res) => {
-    // if the client attempts to change the `owner` property by including a new
-    // owner, prevent that by deleting that key/value pair
-    delete req.body.dog.owner
-
-    Dog.findById(req.params.id)
+  Dog.findById(req.params.id)
     .then(handle404)
     .then(dog => {
       // pass the `req` object and the Mongoose record to `requireOwnership`
@@ -199,12 +202,12 @@ console.log('forthconddog', dogDataDisplayReady())
     .then(() => res.sendStatus(204))
     // if an error occurs, pass it to the handler
     .catch(err => handle(err, res))
-  })
+})
 
-  // DESTROY
-  // DELETE /examples/5a7db6c74d55bc51bdf39793
-  router.delete('/dogs/:id', requireToken, (req, res) => {
-    Dog.findById(req.params.id)
+// DESTROY
+// DELETE /examples/5a7db6c74d55bc51bdf39793
+router.delete('/dogs/:id', requireToken, (req, res) => {
+  Dog.findById(req.params.id)
     .then(handle404)
     .then(dog => {
       // throw an error if current user doesn't own `dog`
@@ -216,6 +219,6 @@ console.log('forthconddog', dogDataDisplayReady())
     .then(() => res.sendStatus(204))
     // if an error occurs, pass it to the handler
     .catch(err => handle(err, res))
-  })
+})
 
-  module.exports = router
+module.exports = router
